47eb8753ef23392ffb864fa4c888041d93f45c1e
Giancarlo Bautista <gbautis@gmail.com>
2015-02-21 23:01:57 -0500

    Replace tabs with spaces in FoodsController

--- public/controllers/FoodsController.js
+++ public/controllers/FoodsController.js
@@ -1,100 +1,100 @@
 'use strict';
 
 app.controller('FoodsController', ['$scope', '$filter', 'foodService', 'MealService', 'DateService', function($scope, $filter, foodService, MealService, DateService) {
-	// for new food inputs
-	$scope.showNewFood = false;
+  // for new food inputs
+  $scope.showNewFood = false;
 
-	$scope.currentMealDate = DateService.getDateNow();
-	$scope.currentMeal = getMeal($scope.currentMealDate);
+  $scope.currentMealDate = DateService.getDateNow();
+  $scope.currentMeal = getMeal($scope.currentMealDate);
 
-	init();
+  init();
 
-	function init() {
+  function init() {
     getFoods();
-		createWatch();
-	}
+    createWatch();
+  }
 
-	function createWatch() {
-		$scope.$watch('nameFilterValue', function (filterInput) {
+  function createWatch() {
+    $scope.$watch('nameFilterValue', function (filterInput) {
       filterFoods(filterInput);
     });
 
     $scope.$watchCollection('currentMeal', function(newValue, oldValue) {
-			calculateTotals();
-		});
-	}
+      calculateTotals();
+    });
+  }
 
   function getFoods() {
     foodService.getFoods().then(
-      function(data) {
-        $scope.foods = data;
-        $scope.filteredFoods = $scope.foods;
-      },
-      function(error) {
-        console.log('ERROR! ' + error);
-      });
+        function(data) {
+          $scope.foods = data;
+          $scope.filteredFoods = $scope.foods;
+        },
+        function(error) {
+          console.log('ERROR! ' + error);
+        });
   }
 
-	function filterFoods(filterInput) {
-		$scope.filteredFoods = $filter('foodFilter')($scope.foods, filterInput);
-	}
+  function filterFoods(filterInput) {
+    $scope.filteredFoods = $filter('foodFilter')($scope.foods, filterInput);
+  }
 
-	function getMeal(date) {
-		return foodService.getMeal(date);
-	}
+  function getMeal(date) {
+    return foodService.getMeal(date);
+  }
 
-	function calculateTotals() {
+  function calculateTotals() {
     // should MealService.calculateTotals return a meal object?
     $scope.currentMeal.totals = MealService.calculateTotals($scope.currentMeal);
-	}
+  }
 
-	$scope.addNewFood = function() {
-		if($scope.newFood !== undefined &&
-		   $scope.newFood.name !== undefined &&
-		   $scope.newFood.cals !== undefined &&
-		   $scope.newFood.protein !== undefined &&
-		   $scope.newFood.carbs !== undefined &&
-		   $scope.newFood.fat !== undefined) {
+  $scope.addNewFood = function() {
+    if($scope.newFood !== undefined &&
+        $scope.newFood.name !== undefined &&
+        $scope.newFood.cals !== undefined &&
+        $scope.newFood.protein !== undefined &&
+        $scope.newFood.carbs !== undefined &&
+        $scope.newFood.fat !== undefined) {
 
-			foodService.addNewFood($scope.newFood);
+      foodService.addNewFood($scope.newFood);
 
-			$scope.newFood = {};
-		} else {
-			alert('must input food info');
-		}
-	};
+      $scope.newFood = {};
+    } else {
+      alert('must input food info');
+    }
+  };
 
-	$scope.addFoodToMeal = function() {
+  $scope.addFoodToMeal = function() {
     MealService.addFoodToMeal($scope.currentMeal, this.food);
-	};
+  };
 
   // update macros after servings input change
-	$scope.updateMacros = function(food) {
+  $scope.updateMacros = function(food) {
     MealService.updateMacros(food);
-		calculateTotals();
-	}
+    calculateTotals();
+  }
 
-	$scope.increaseServing = function(food) {
+  $scope.increaseServing = function(food) {
     // FIXME: cannot have duplicate keys (foods) in ng-repeat
     // this breaks if you try and decrease serving on a duplicate food
     MealService.increaseServing($scope.currentMeal, food);
     calculateTotals();
-	};
+  };
 
-	$scope.decreaseServing = function(food) {
+  $scope.decreaseServing = function(food) {
     // FIXME: cannot have duplicate keys (foods) in ng-repeat
     // this breaks if you try and decrease serving on a duplicate food
     MealService.decreaseServing($scope.currentMeal, food);
     calculateTotals();
-	};
+  };
 
-	$scope.incrementDay = function() {
+  $scope.incrementDay = function() {
     DateService.incrementDay($scope.currentMealDate);
     $scope.currentMeal = getMeal($scope.currentMealDate);
-	}
+  }
 
-	$scope.decrementDay = function() {
+  $scope.decrementDay = function() {
     DateService.decrementDay($scope.currentMealDate);
     $scope.currentMeal = getMeal($scope.currentMealDate);
-	};
+  };
 }]);
